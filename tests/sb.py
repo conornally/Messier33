836dbf42cf9b8dc97c64ca765de98fd2ee0fc14f
conornally <conor.nally@hotmail.com>
2020-02-05 16:04:36 +0000

    first correct isophot fitting instance

--- tests/sb.py
+++ tests/sb.py
@@ -5,8 +5,8 @@ from photutils.datasets import make_noise_image
 g = Gaussian2D(100., 75, 75, 20, 12, theta=40.*np.pi/180.)
 ny = nx = 150
 y, x = np.mgrid[0:ny, 0:nx]
-noise = make_noise_image((ny, nx), distribution='gaussian', mean=0., stddev=2., random_state=12345)
-data = g(x, y) + noise
+#noise = make_noise_image((ny, nx), distribution='gaussian', mean=0., stddev=2., random_state=12345)
+data = g(x, y) #+ noise
 
 from photutils.isophote import EllipseGeometry
 geometry = EllipseGeometry(x0=75, y0=75, sma=20, eps=0.5, pa=20.*np.pi/180.)
@@ -14,12 +14,12 @@ geometry = EllipseGeometry(x0=75, y0=75, sma=20, eps=0.5, pa=20.*np.pi/180.)
 from photutils import EllipticalAperture
 aper = EllipticalAperture((geometry.x0, geometry.y0), geometry.sma, geometry.sma*(1 - geometry.eps), geometry.pa)
 
-#plt.imshow(data)
-#aper.plot()
-#plt.show()
+plt.imshow(data)
+aper.plot()
 
 from photutils.isophote import Ellipse
 ellipse = Ellipse(data, geometry)
 
 isolist = ellipse.fit_image()
 print(isolist.pa)
+plt.show()
--- tests/sbreale.py
+++ tests/sbreale.py
@@ -1,43 +1,39 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from astropy.io import fits
 from photutils import isophote
 from photutils.isophote import Ellipse
 from photutils import EllipticalAperture
 from Messier33 import inclination
-import astropy.visualization as vis
 
-with fits.open("/mnt/sd/data/m33/ellipse/m33_i_mosaic.fits") as d:
+with fits.open("/home/s1539878/data/mphys/ellipse/m33_i_mosaic.fits") as d:
     head=d[0].header
     data=d[0].data
 
-#print(repr(head))
 
 x0=head["CRPIX1"]
 y0=head["CRPIX2"]
 
-x1,y1 = np.unravel_index(np.argmax(data), data.shape)
+#x1,y1 = np.unravel_index(np.argmax(data), data.shape)
 x2,y2=(3000,2600)
-PA=np.radians(head["ROTSKYPA"])
-sma=200
-e=sma*(1-np.cos(inclination))
-print(PA, head["ROTSKYPA"])
+PA=np.radians(135)
+sma=500
+e=np.cos(inclination)
+print(e)
 geometry = isophote.EllipseGeometry(x2, y2, sma, e, PA)
-aper=EllipticalAperture((x2,y2),sma, e, PA) 
-#aper = EllipticalAperture((geometry.x0, geometry.y0), geometry.sma, geometry.sma*(1 - geometry.eps), geometry.pa)
-fig,ax = plt.subplots(1)
-ax.imshow(data)
-ax.scatter(x0,y0)
-ax.scatter(x1,y1)
-ax.scatter(y1,x1)
-ax.scatter(x2,y2)
-aper.plot(ax)
-print(aper)
-print(data[x1,y1])
-plt.show()
-#quit()
+print(geometry.__dict__)
+#aper=EllipticalAperture((x2,y2),sma, sma*(1-e), PA) 
 
-ellipse = Ellipse(data, geometry)
-print(ellipse.fit_isophote(geometry.sma))
-#isolist=ellipse.fit_image(100,0,1000)
+"""
+fig,ax = plt.subplots(1)
+plt.imshow(data)
+plt.scatter(x2,y2)
+aper.plot()
+plt.show()
+"""
+ellipse = Ellipse(data, geometry, threshold=0.01)
+#print(ellipse.fit_isophote(400))
+isolist=ellipse.fit_image(400,0,2000)
+print(isolist)
+isolist.to_table().to_pandas().to_csv("tmp")
+
 
